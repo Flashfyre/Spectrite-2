65c4fb22a6f11c6db3b5a8e12eb811b18e6d3246
Flashfyre <flashfireex@gmail.com>
2022-07-07 10:57:59 -0400

    Update Fabric API and dynamic loot tables logic

---
+++ src/main/java/com/flashfyre/spectrite/mixin/LootPoolBuilderAccessor.java
@@ -0,0 +1,15 @@
+package com.flashfyre.spectrite.mixin;
+
+import net.minecraft.loot.LootPool;
+import net.minecraft.loot.entry.LootPoolEntry;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+import java.util.List;
+
+@Mixin(LootPool.Builder.class)
+public interface LootPoolBuilderAccessor
+{
+    @Accessor("entries")
+    List<LootPoolEntry> getEntries();
+}
